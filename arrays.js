cb03da1526ab15393228e46c3a2efe43b8d96beb
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-17 20:59:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,6 +32,7 @@ function addElementToEndOfArray(array, element){
   }
 
   function accessElementInArray (array, index){
+      var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         console.log(array[3])
   }
 
