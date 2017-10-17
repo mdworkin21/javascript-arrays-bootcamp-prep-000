af076ccf1e1e49de09c2efbc38e6816d9c2b1508
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-17 21:00:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,8 +32,8 @@ function addElementToEndOfArray(array, element){
   }
 
   function accessElementInArray (array, index){
-      var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        console.log(array[3])
+      var array = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+        console.log(array[0])
   }
 
   function destructivelyRemoveElementFromBeginningOfArray(array){
