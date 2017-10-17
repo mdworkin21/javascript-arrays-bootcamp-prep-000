4fc8f053408f5f6a114ba6df8f47babc43be3264
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-17 19:59:33 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,60 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = [1]
+  newArray = ["foo", ...newArray]
+  return newArray
+  }
+  
+
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  var array = ["foo", 1]
+  array.unshift()
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  var array = [1, "foo"]
+  array.push()
+  return array
+}
+
+function addElementToEndOfArray(array, element){
+  var newArray = [1]
+  newArray = [...newArray, "foo"]
+  return newArray
+  }
+
+  function accessElementInArray (array, index){
+        console.log(array[3])
+  }
+
+  function destructivelyRemoveElementFromBeginningOfArray(array){
+    var array = [1, 2, 3]
+    array.shift()
+    return array
+  }
+
+  function removeElementFromBeginningOfArray(array){
+    var array = [2, 3]
+    array.slice(-1)
+    return array
+  }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  var array = [1, 2, 3]
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+    var array = [1, 2]
+    array.slice(0, array.length - 1)
+    return array
+  }
