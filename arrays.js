86695a19bda1a302966f55c586a1297aa0126075
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-17 21:19:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function addElementToBeginningOfArray(array, element){
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   var array = [1]
-  array.unshift(0 array.length -1, "foo")
+  array.unshift("foo")
   return array
 }
 
